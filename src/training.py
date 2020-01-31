e75985a7552e19617594f0bdeebc563be62cef31
Nicolas Douard <nicolasdouard@gmail.com>
2020-01-30 20:17:43 -0500

    Upgrade training.py

--- src/training.py
+++ src/training.py
@@ -38,68 +38,125 @@ def stretch_8bit(bands, lower_percent=2, higher_percent=98):
         out[:,:,i] =t
     return out.astype(np.uint8)
 
-def get_unet(n_ch,patch_height,patch_width):
-    concat_axis = 3
+def get_unet(lr=1e-4, deep=True, dims=20, conv_channel=32, N_Cls=10, bn=False, use_sample_weights=True,
+             init="glorot_uniform", use_jaccard_loss=False):
+    """
+    Creates the U-Net in Keras
+    """
+    inputs = Input((dims, CROP_SIZE, CROP_SIZE))
+    conv1 = Convolution2D(conv_channel, 3, 3, border_mode='same', init=init)(inputs)
+    if bn:
+        conv1 = BatchNormalization()(conv1)
+    conv1 = Activation("relu")(conv1)
+    conv1 = Convolution2D(conv_channel, 3, 3, border_mode='same', init=init)(conv1)
+    if bn:
+        conv1 = BatchNormalization()(conv1)
+    conv1 = Activation("relu")(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    inputs = Input((patch_height, patch_width, n_ch))
-    
-    conv1 = Conv2D(32, (3, 3), padding="same", name="conv1_1", activation="relu", data_format="channels_last")(inputs)
-    conv1 = Conv2D(32, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv1)
-    conv2 = Conv2D(64, (3, 3), padding="same", activation="relu", data_format="channels_last")(pool1)
-    conv2 = Conv2D(64, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv2)
+    conv2 = Convolution2D(conv_channel * 2, 3, 3, border_mode='same', init=init)(pool1)
+    if bn:
+        conv2 = BatchNormalization()(conv2)
+    conv2 = Activation("relu")(conv2)
+    conv2 = Convolution2D(conv_channel * 2, 3, 3, border_mode='same', init=init)(conv2)
+    if bn:
+        conv2 = BatchNormalization()(conv2)
+    conv2 = Activation("relu")(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Conv2D(128, (3, 3), padding="same", activation="relu", data_format="channels_last")(pool2)
-    conv3 = Conv2D(128, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv3)
+    conv3 = Convolution2D(conv_channel * 4, 3, 3, border_mode='same', init=init)(pool2)
+    if bn:
+        conv3 = BatchNormalization()(conv3)
+    conv3 = Activation("relu")(conv3)
+    conv3 = Convolution2D(conv_channel * 4, 3, 3, border_mode='same', init=init)(conv3)
+    if bn:
+        conv3 = BatchNormalization()(conv3)
+    conv3 = Activation("relu")(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Conv2D(256, (3, 3), padding="same", activation="relu", data_format="channels_last")(pool3)
-    conv4 = Conv2D(256, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv4)
+    conv4 = Convolution2D(conv_channel * 8, 3, 3, border_mode='same', init=init)(pool3)
+    if bn:
+        conv4 = BatchNormalization()(conv4)
+    conv4 = Activation("relu")(conv4)
+    conv4 = Convolution2D(conv_channel * 8, 3, 3, border_mode='same', init=init)(conv4)
+    if bn:
+        conv4 = BatchNormalization()(conv4)
+    conv4 = Activation("relu")(conv4)
+    if deep:
+        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Conv2D(512, (3, 3), padding="same", activation="relu", data_format="channels_last")(pool4)
-    conv5 = Conv2D(512, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv5)
+        conv5 = Convolution2D(conv_channel * 16, 3, 3, border_mode='same', init=init)(pool4)
+        if bn:
+            conv5 = BatchNormalization()(conv5)
+        conv5 = Activation("relu")(conv5)
+        conv5 = Convolution2D(conv_channel * 16, 3, 3, border_mode='same', init=init)(conv5)
+        if bn:
+            conv5 = BatchNormalization()(conv5)
+        conv5 = Activation("relu")(conv5)
 
-    up_conv5 = UpSampling2D(size=(2, 2), data_format="channels_last")(conv5)
-    ch, cw = get_crop_shape(conv4, up_conv5)
-    crop_conv4 = Cropping2D(cropping=(ch,cw), data_format="channels_last")(conv4)
-    up6   = concatenate([up_conv5, crop_conv4], axis=concat_axis)
-    conv6 = Conv2D(256, (3, 3), padding="same", activation="relu", data_format="channels_last")(up6)
-    conv6 = Conv2D(256, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv6)
+        up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
+        conv6 = Convolution2D(conv_channel * 8, 3, 3, border_mode='same', init=init)(up6)
+        if bn:
+            conv6 = BatchNormalization()(conv6)
+        conv6 = Activation("relu")(conv6)
+        conv6 = Convolution2D(conv_channel * 8, 3, 3, border_mode='same', init=init)(conv6)
+        if bn:
+            conv6 = BatchNormalization()(conv6)
+        conv6 = Activation("relu")(conv6)
 
-    up_conv6 = UpSampling2D(size=(2, 2), data_format="channels_last")(conv6)
-    ch, cw = get_crop_shape(conv3, up_conv6)
-    crop_conv3 = Cropping2D(cropping=(ch,cw), data_format="channels_last")(conv3)
-    up7   = concatenate([up_conv6, crop_conv3], axis=concat_axis)
-    conv7 = Conv2D(128, (3, 3), padding="same", activation="relu", data_format="channels_last")(up7)
-    conv7 = Conv2D(128, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv7)
+        up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
+    else:
+        up7 = merge([UpSampling2D(size=(2, 2))(conv4), conv3], mode='concat', concat_axis=1)
+    conv7 = Convolution2D(conv_channel*4, 3, 3, border_mode='same', init=init)(up7)
+    if bn:
+        conv7 = BatchNormalization()(conv7)
+    conv7 = Activation("relu")(conv7)
+    conv7 = Convolution2D(conv_channel*4, 3, 3, border_mode='same', init=init)(conv7)
+    if bn:
+        conv7 = BatchNormalization()(conv7)
+    conv7 = Activation("relu")(conv7)
 
-    up_conv7 = UpSampling2D(size=(2, 2), data_format="channels_last")(conv7)
-    ch, cw = get_crop_shape(conv2, up_conv7)
-    crop_conv2 = Cropping2D(cropping=(ch,cw), data_format="channels_last")(conv2)
-    up8   = concatenate([up_conv7, crop_conv2], axis=concat_axis)
-    conv8 = Conv2D(64, (3, 3), padding="same", activation="relu", data_format="channels_last")(up8)
-    conv8 = Conv2D(64, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv8)
+    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
+    conv8 = Convolution2D(conv_channel*2, 3, 3, border_mode='same', init=init)(up8)
+    if bn:
+        conv8 = BatchNormalization()(conv8)
+    conv8 = Activation("relu")(conv8)
+    conv8 = Convolution2D(conv_channel*2, 3, 3, border_mode='same', init=init)(conv8)
+    if bn:
+        conv8 = BatchNormalization()(conv8)
+    conv8 = Activation("relu")(conv8)
 
-    up_conv8 = UpSampling2D(size=(2, 2), data_format="channels_last")(conv8)
-    ch, cw = get_crop_shape(conv1, up_conv8)
-    crop_conv1 = Cropping2D(cropping=(ch,cw), data_format="channels_last")(conv1)
-    up9   = concatenate([up_conv8, crop_conv1], axis=concat_axis)
-    conv9 = Conv2D(32, (3, 3), padding="same", activation="relu", data_format="channels_last")(up9)
-    conv9 = Conv2D(32, (3, 3), padding="same", activation="relu", data_format="channels_last")(conv9)
+    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
+    conv9 = Convolution2D(conv_channel, 3, 3, border_mode='same', init=init)(up9)
+    if bn:
+        conv9 = BatchNormalization()(conv9)
+    conv9 = Activation("relu")(conv9)
+    conv9 = Convolution2D(conv_channel, 3, 3, border_mode='same', init=init)(conv9)
+    if bn:
+        conv9 = BatchNormalization()(conv9)
+    conv9 = Activation("relu")(conv9)
 
-    #ch, cw = get_crop_shape(inputs, conv9)
-    #conv9  = ZeroPadding2D(padding=(ch[0],cw[0]), data_format="channels_last")(conv9)
-    #conv10 = Conv2D(1, (1, 1), data_format="channels_last", activation="sigmoid")(conv9)
-    
-    flatten =  Flatten()(conv9)
-    Dense1 = Dense(512, activation='relu')(flatten)
-    BN =BatchNormalization() (Dense1)
-    Dense2 = Dense(17, activation='sigmoid')(BN)
-    
-    model = Model(input=inputs, output=Dense2)
-    
+    # Output Convolution Layer with 10 binary classes
+    outmap = Convolution2D(N_Cls, 1, 1, activation='sigmoid')(conv9)
+    if use_sample_weights:
+        outmap = Reshape((N_Cls, 160*160))(outmap)
+        outmap = Permute((2,1))(outmap)
+
+    model = Model(input=inputs, output=outmap)
+    # optimizer = RMSprop(lr=0.045, rho=0.9, epsilon=1e-8)
+    optimizer = Adam(lr=lr)
+    if use_sample_weights:
+        if use_jaccard_loss:
+            model.compile(optimizer=optimizer, loss=jaccard_loss, metrics=['accuracy', jaccard_coef],
+                          sample_weight_mode="temporal")
+        else:
+            model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy', jaccard_coef],
+                          sample_weight_mode="temporal")
+    else:
+        if use_jaccard_loss:
+            model.compile(optimizer=optimizer, loss=jaccard_loss, metrics=['accuracy', jaccard_coef])
+        else:
+            model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy', jaccard_coef])
     return model
 
 def calc_jacc(model, logger, dims, visual_name, img, msk, use_sample_weights, N_Cls=10):
